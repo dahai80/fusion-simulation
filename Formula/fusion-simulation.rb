8055c345d2c833bd3bf66bab3c7715263b126343
dahai80 <dahai80@users.noreply.github.com>
2026-08-07 19:10:46 +0800

    chore(formula): pin sha256 to published v0.1.6 tarball hash

--- Formula/fusion-simulation.rb
+++ Formula/fusion-simulation.rb
@@ -2,7 +2,7 @@ class FusionSimulation < Formula
   desc "Robot virtual simulation training and testing platform for Apple Silicon"
   homepage "https://github.com/dahai80/fusion-simulation"
   url "https://github.com/dahai80/fusion-simulation/archive/refs/tags/v0.1.6.tar.gz"
-  sha256 "6fa0de5a164d39ff676e0852a0e2b0d6811db787ae7be8ce740657bc8113a091"
+  sha256 "28c437576ac0c2b5dd39dfcbb694da953fcbb7d7d6158a201ac876421b621a22"
   license "Apache-2.0"
   head "https://github.com/dahai80/fusion-simulation.git", branch: "main"
 
